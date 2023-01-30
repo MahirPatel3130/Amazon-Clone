47b25924e6b4a15293ef6ac1ab739fd44bf5d717
MahirPatel3130 <117554519+MahirPatel3130@users.noreply.github.com>
2023-01-30 01:03:20 -0800

    Add files via upload

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Product from "./Product";
+
+function ProductFeed({ products }) {
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-col-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 3)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+
+      {products
+        .slice(4, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default ProductFeed;
